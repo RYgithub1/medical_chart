a573fbea67b03e8a13f7dbd1a3f61b60866c57aa
RYgithub1 <ryo.rec.otani@gmail.com>
2020-11-29 22:10:29 +0900

    Implement fontAwsome

--- lib/fruits_heaven_page.dart
+++ lib/fruits_heaven_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:uuid/uuid.dart';
 
 
@@ -44,6 +45,7 @@ class _FruitsHeavenPageState extends State<FruitsHeavenPage>{
 
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomPadding: false,
         appBar: AppBar(
           leading: IconButton(
             icon: Icon(Icons.arrow_back),
@@ -56,11 +58,42 @@ class _FruitsHeavenPageState extends State<FruitsHeavenPage>{
         endDrawer: Drawer(
           child: Container(),
         ),
-        body: InteractiveViewer(
-          minScale: 0.4,
-          maxScale: 3.2,
-          constrained: false,
-          child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRoP5IzALZ8GLCt0w0-Rqj7AnRt_8U4-oGAXA&usqp=CAU"),
+        body: Column(
+          children: <Widget>[
+            SizedBox(height:40),
+            // InteractiveViewer(
+            //   minScale: 0.4,
+            //   maxScale: 3.2,
+            //   constrained: false,
+            //   child: Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRoP5IzALZ8GLCt0w0-Rqj7AnRt_8U4-oGAXA&usqp=CAU"),
+            // ),
+            Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRoP5IzALZ8GLCt0w0-Rqj7AnRt_8U4-oGAXA&usqp=CAU"),
+            SizedBox(height:40),
+            Text("Any Order with SNS Auth",  style:TextStyle(fontWeight:FontWeight.bold, fontSize:32)),
+            Divider(height:4, thickness:4, indent:32, endIndent:32),
+            SizedBox(height:20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  icon: FaIcon(FontAwesomeIcons.google, color:Colors.red),
+                  onPressed: (){print("SNS/Google");},
+                ),
+                IconButton(
+                  icon: FaIcon(FontAwesomeIcons.facebook, color:Colors.blue),
+                  onPressed: (){print("SNS/Facebook");},
+                ),
+                IconButton(
+                  icon: FaIcon(FontAwesomeIcons.apple, color:Colors.green),
+                  onPressed: (){print("SNS/Apple");},
+                ),
+                IconButton(
+                  icon: FaIcon(FontAwesomeIcons.amazon, color:Colors.orange),
+                  onPressed: (){print("SNS/Amazon");},
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
